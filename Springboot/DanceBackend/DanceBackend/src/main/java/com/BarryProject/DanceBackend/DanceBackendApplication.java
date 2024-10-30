a4e637bffd239e9e309e651329741271b66173a9
barryismaila4 <ismaila.barry@gmail.com>
2024-10-30 01:42:02 +0100

    angular et springboot

---
+++ Springboot/DanceBackend/DanceBackend/src/main/java/com/BarryProject/DanceBackend/DanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.BarryProject.DanceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceBackendApplication.class, args);
+	}
+
+}
---
+++ Springboot/DanceBackend/DanceBackend/src/test/java/com/BarryProject/DanceBackend/DanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarryProject.DanceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
